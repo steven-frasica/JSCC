aa71f427e21f3ce88a55782e6fefd2aa33cfcb84
steven-frasica <steven.frasica@gmail.com>
2025-12-10 22:16:40 -0500

    Add console log on data types

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Hello"[0]);
+console.log("Hello"[4]);
+console.log("Hello".charAt("Hello".length - 1));
+console.log("12345".length)
+
+let str = "Steven";
+console.log(str[0]);
+console.log(str[str.length - 1]);
+console.log(str[5 - 1]);
+console.log(2 * 10);
+console.log(10/2);
